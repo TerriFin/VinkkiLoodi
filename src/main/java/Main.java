865bf5491f5c74fac1124ced955ba9e8bc17684f
TerriFin <saukkonensami@gmail.com>
2018-11-15 13:48:54 +0200

    Made baseline gradle build

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author samisaukkonen
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
