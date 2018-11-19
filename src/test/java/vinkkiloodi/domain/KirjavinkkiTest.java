ca3421b30ee07de9d427a81b04e2ac02eacbcb84
JoonaHa <joona.halonen@helsinki.fi>
2018-11-19 23:17:15 +0200

    UI kysyy ja näyttää ISBN

--- src/test/java/vinkkiloodi/domain/KirjavinkkiTest.java
+++ src/test/java/vinkkiloodi/domain/KirjavinkkiTest.java
@@ -1,8 +1,6 @@
 
 import vinkkiloodi.domain.Kirjavinkki;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
