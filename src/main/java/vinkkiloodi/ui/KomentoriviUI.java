5ed1dbeb1868410ebbb69ee29272aac3f0086cb1
Thomas Stenius <thomas.stenius@helsinki.fi>
2018-11-19 18:54:21 +0200

    UI muutoksia

--- src/main/java/vinkkiloodi/ui/KomentoriviUI.java
+++ src/main/java/vinkkiloodi/ui/KomentoriviUI.java
@@ -1,4 +1,3 @@
-
 package vinkkiloodi.ui;
 
 import java.util.ArrayList;
@@ -12,79 +11,79 @@ import vinkkiloodi.domain.Vinkki;
  *
  * @author tumppi
  */
-
 public class KomentoriviUI {
-    Scanner scanner;
+
+    Scanner input;
     VinkkiDAO dao;
 
-    public KomentoriviUI(Scanner scanner, VinkkiDAO dao) {
-        this.scanner = scanner;
+    public KomentoriviUI(Scanner input, VinkkiDAO dao) {
+        this.input = input;
         this.dao = dao;
     }
-    
+
     public void start() {
-        System.out.println("Tervetuloa vinkkiloodiin!\n----------------------"
+        System.out.println("Tervetuloa vinkkiloodiin!\n----------------------\n"
                 + "Vinkkiloodi on lukuvinkkikirjasto, johon voit lisätä lukuvinkkejä.\n");
         paavalikko();
     }
-    
+
     public void paavalikko() {
-        System.out.println("\nMitä haluat tehdä?"
-                + "\n1 - Lisää vinkki"
-                + "\n2 - Listaa vinkit"
-                + "\n3 - Merkitse vinkki luetuksi"
-                + "\nX - Sammuta ohjelma");
-        
-        String komento = scanner.nextLine();
-        
-        if (komento.toLowerCase().equals("x")) {
-            System.exit(0);
-        } else if (komento.equals("1")) {
-            lisaaVinkki();
-        } else if (komento.equals("2")) {
-            listaaKaikki();
-        } else if (komento.equals("3")) {
-            merkitseVinkkiLuetuksi();
+        while (true) {
+            System.out.println("\nMitä haluat tehdä?"
+                    + "\n1 - Lisää vinkki"
+                    + "\n2 - Listaa vinkit"
+                    + "\n3 - Merkitse vinkki luetuksi"
+                    + "\nX - Sammuta ohjelma");
+
+            String komento = input.nextLine();
+
+            if (komento.toLowerCase().equals("x")) {
+                break;
+            } else if (komento.equals("1")) {
+                lisaaVinkki();
+            } else if (komento.equals("2")) {
+                listaaKaikki();
+            } else if (komento.equals("3")) {
+                merkitseVinkkiLuetuksi();
+            } else {
+                System.out.println("\nVirheellinen komento.");
+            }
         }
+        System.exit(0);
     }
-    
-    
+
     public void lisaaVinkki() {
         System.out.println("Lisää vinkki\n-----------\n");
         System.out.print("Nimi: ");
-        String nimi = scanner.nextLine();
+        String nimi = input.nextLine();
         System.out.print("Otsikko: ");
-        String otsikko = scanner.nextLine();
+        String otsikko = input.nextLine();
 
         dao.add(new Kirjavinkki(nimi, otsikko));
-        paavalikko();
     }
-    
+
     public void listaaKaikki() {
         System.out.println("Kaikki vinkit\n------------\n");
-        List<Vinkki> vinkit = new ArrayList<>(); 
-        
+        List<Vinkki> vinkit = new ArrayList<>();
+
         //TODO daosta saatu lista vinkit = dao.esimesim();
-        
         for (int i = 0; i < vinkit.size(); i++) {
             System.out.println(vinkit.get(i));
         }
-        paavalikko();
     }
-    
+
     public void merkitseVinkkiLuetuksi() {
         System.out.println("Merkitse vinkki luetuksi\n--------------------\n");
         System.out.print("Syötä lukuvinkin id: ");
-        int hakuId = Integer.parseInt(scanner.nextLine());
-        
+        int hakuId = Integer.parseInt(input.nextLine());
+
         // TODO haku tietokannasta daon avulla.
         Vinkki haettu = dao.getById(hakuId);
-        
+
         if (haettu != null) {
             System.out.println("Vinkki " + hakuId + " merkittiin luetuksi.");
         } else {
             System.out.println("Vinkki " + hakuId + " ei löytynyt järjestelmästä.");
         }
-        paavalikko();
     }
 }
