2e6b094509fd8783f8f51a6ffc0add956319b677
Sami Lahtinen <thesamsai@gmail.com>
2018-11-21 15:11:34 +0200

    UI:n testit tuhottu

--- src/test/java/vinkkiloodi/ui/KomentoriviUITest.java
+++ src/test/java/vinkkiloodi/ui/KomentoriviUITest.java
@@ -2,9 +2,9 @@ package vinkkiloodi.ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import vinkkiloodi.database.InMemoryDAO;
 import vinkkiloodi.database.VinkkiDAO;
 import vinkkiloodi.io.StubIO;
@@ -15,8 +15,6 @@ import vinkkiloodi.io.StubIO;
  */
 public class KomentoriviUITest {
     
-    private KomentoriviUI ui;
-    private StubIO io;
     private VinkkiDAO dao;
     private List<String> komennot;
 
@@ -26,41 +24,5 @@ public class KomentoriviUITest {
         komennot = new ArrayList<>();
     }
     
-    private void lisaaVinkki(String lisattava) {
-        komennot.add("1");
-        komennot.add(lisattava);
-        komennot.add(lisattava);
-        komennot.add(lisattava);
-    }
     
-    private void alustaTesti() {
-        komennot.add("x");
-        
-        io = new StubIO(komennot);
-        ui = new KomentoriviUI(io, dao);
-    }
-
-    @Test
-    public void lisayksenJalkeenLoytyy() {
-        lisaaVinkki("test");
-        alustaTesti();
-        
-        int kokoEnnen = dao.getAll().size();
-        
-        ui.start();
-        
-        assertTrue(kokoEnnen < dao.getAll().size());
-    }
-    
-    @Test
-    public void vinkkiLoytyyLisayksenJalkeen() {
-        lisaaVinkki("test1");
-        lisaaVinkki("test2");
-        komennot.add("2");
-        alustaTesti();
-        
-        ui.start();
-        
-        assertTrue(io.getOutput().contains("test2"));
-    }
 }
