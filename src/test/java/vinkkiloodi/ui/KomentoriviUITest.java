d06c07c95dfc7702e50aa39bc87edeef6217b6fa
JoonaHa <joona.halonen@helsinki.fi>
2018-11-24 23:47:03 +0200

    Aartikkeli ja Blogi UI-testit lisätty

--- src/test/java/vinkkiloodi/ui/KomentoriviUITest.java
+++ src/test/java/vinkkiloodi/ui/KomentoriviUITest.java
@@ -33,9 +33,26 @@ public class KomentoriviUITest {
 
     private void lisaaTestiKirja(String kirjoittaja, String otsikko, String ISBN) {
         input.add("1");
+        input.add("1");
         input.add(kirjoittaja);
         input.add(otsikko);
         input.add(ISBN);
+    }
+
+    private void lisaaTestiBlogi(String kirjoittaja, String otsikko, String url) {
+        input.add("1");
+        input.add("2");
+        input.add(kirjoittaja);
+        input.add(otsikko);
+        input.add(url);
+    }
+
+    private void lisaaTestiArtikkeli(String kirjoittaja, String otsikko) {
+        input.add("1");
+        input.add("3");
+        input.add(kirjoittaja);
+        input.add(otsikko);
+
     }
 
     private void aloitaOhjelma() {
@@ -49,9 +66,31 @@ public class KomentoriviUITest {
         lisaaTestiKirja("kirjoittaja", "kirja", "12345");
 
         int vinkkejaEnnen = dao.getAll().size();
-        
+
         aloitaOhjelma();
-        
+
+        assertTrue(vinkkejaEnnen < dao.getAll().size());
+    }
+
+    @Test
+    public void voiLisataBlogiVinkin() {
+        lisaaTestiBlogi("kirjoittaja", "blogiposti", "https://olematontestiurl.ei");
+
+        int vinkkejaEnnen = dao.getAll().size();
+
+        aloitaOhjelma();
+
+        assertTrue(vinkkejaEnnen < dao.getAll().size());
+    }
+
+    @Test
+    public void voiLisataArtikkeliVinkin() {
+        lisaaTestiArtikkeli("kirjoittaja", "artikkeli");
+
+        int vinkkejaEnnen = dao.getAll().size();
+
+        aloitaOhjelma();
+
         assertTrue(vinkkejaEnnen < dao.getAll().size());
     }
 }
