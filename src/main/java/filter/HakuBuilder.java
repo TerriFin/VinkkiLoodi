5ecbbab99ee4bc9fbaed7e88e198509639ead725
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2018-12-12 17:13:31 +0200

    Pikakomentojen et ja kt täydennys

--- src/main/java/filter/HakuBuilder.java
+++ src/main/java/filter/HakuBuilder.java
@@ -12,63 +12,71 @@ import vinkkiloodi.domain.Tyyppi;
  * @author sami
  */
 public class HakuBuilder {
+
     private Matcher haku;
-    
+
     public HakuBuilder() {
         haku = new Kaikki();
     }
-    
+
     public Matcher build() {
         Matcher uusiHaku = haku;
-        
+
         haku = new Kaikki();
-        
+
         return uusiHaku;
     }
-    
+
     public HakuBuilder onTyyppia(Tyyppi tyyppi) {
         haku = new And(haku,
-                    new OnTyyppia(tyyppi)
+                new OnTyyppia(tyyppi)
         );
-        
+
         return this;
     }
-    
+
     public HakuBuilder nimiSisaltaa(String teksti) {
         haku = new And(haku,
-                    new NimiSisaltaa(teksti)
+                new NimiSisaltaa(teksti)
         );
-        
+
         return this;
     }
-    
+
     public HakuBuilder tekijaSisaltaa(String teksti) {
         haku = new And(haku,
-                    new TekijaSisaltaa(teksti)
+                new TekijaSisaltaa(teksti)
         );
-        
+
         return this;
     }
-    
+
     public HakuBuilder tarkastettu() {
         haku = new And(haku,
-                    new Tarkastettu()
+                new Tarkastettu()
         );
-        
+
         return this;
     }
-    
+
     public HakuBuilder tarkastamaton() {
         haku = new And(haku,
-                    new Tarkistamaton()
+                new Tarkistamaton()
         );
-        
+
         return this;
     }
-    
+
     public HakuBuilder matchOne(Matcher... matchers) {
         haku = new Or(matchers);
-        
+
+        return this;
+    }
+
+    public HakuBuilder onJokuHalutuistaTyypeista(boolean kirja, boolean artikkeli, boolean blog) {
+        haku = new And(haku,
+                new OnJotainTyypeista(kirja, artikkeli, blog)
+        );
         return this;
     }
 }
